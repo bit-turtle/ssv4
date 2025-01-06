68d99cbe2ecd84cb7586d4ce6c55ac78713f2d08
hotdogz7 ! <mts18142027@gmail.com>
2025-01-06 00:06:04 -0500

    Added ss4as.bat

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # ssv4: Super Seal Version 4
 ssv4 contains two projects
-## ss4as
-ss4as is the assember for the ssv4 cpu
-## ss4vm
-ss4vm is the virtual machine for the ssv4 cpu
+## ss4as: the assember for the ssv4 cpu
+Using ss4as:
+1. ss4as is a command line tool, run ss4as --help for more information
+2. On Windows, `ss4as.bat` can be used to run ss4as with a file selector gui
+## ss4vm: the virtual machine for the ssv4 cpu
+## Building
+All Platforms (Start by opening a terminal in this folder):
+1. `mkdir build`
+2. `cd build`
+3. `cmake ..`
+Windows (Use VS 2022 Terminal):
+1. `MSBuild.exe .\ss4as.vxproj -p:Configuration=Release`
+2. `MSBuild.exe .\ss4vm.vxproj -p:Configuration=Release`
+3. `cd Release`
+4. `cp ..\..\src\ss4as.bat .`
+Linux/Unix:
+1. `make all`
---
+++ src/ss4as.bat
@@ -0,0 +1,34 @@
+:: ss4as Windows Batch File
+
+@echo off
+
+:: Message
+echo ss4as Assembler
+
+:: File Chooser Dialog
+echo Choose Source Files . . .
+
+set files=
+
+for /f "delims=" %%I in ('powershell "Add-Type -AssemblyName System.Windows.Forms;$f = new-object Windows.Forms.OpenFileDialog;$f.InitialDirectory = pwd;$f.Filter = 'All Files (*.*)|*.*';$f.ShowHelp = $true;$f.Multiselect = $true;[void]$f.ShowDialog();if ($f.Multiselect) { $f.FileNames } else { $f.FileName }"') do (
+    set files=%files% %%~I
+)
+
+echo Source Files: %files%
+
+:: Save File Dialog
+echo Choose Output File . . .
+
+set chooser=powershell "Add-Type -AssemblyName System.windows.forms|Out-Null;$f=New-Object System.Windows.Forms.SaveFileDialog;$f.InitialDirectory='%cd%';$f.Filter='All Files (*.*)|*.*';$f.showHelp=$true;$f.ShowDialog()|Out-Null;$f.FileName"
+
+:: capture choice to a variable
+for /f "delims=" %%I in ('%chooser%') do set "filename=%%I"
+
+echo Output File: %filename%
+
+:: Run ss4as
+echo Running ss4as . . .
+.\ss4as.exe %files% -o %file%
+
+:: Wait For Keypress
+pause
